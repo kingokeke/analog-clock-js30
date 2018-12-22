8e60f8689234a1863b7684036c0286dcf9e91848
Kingsley Okeke <kingsleyokeke.ng@gmail.com>
2018-12-22 13:32:06 +0100

    Added JS functionality for animating hour hand and minute hand

--- js/main.js
+++ js/main.js
@@ -2,11 +2,18 @@ const secondHand = document.querySelector('.second-hand');
 const minuteHand = document.querySelector('.minute-hand');
 const hourHand = document.querySelector('.hour-hand');
 
-function animateSecondHand() {
+function analogClock() {
 	const time = new Date();
 	const seconds = time.getSeconds();
+	const minutes = time.getMinutes();
+	const hours = time.getHours();
+
 	const secondsDegree = Math.trunc((seconds / 60) * 360);
+	const minutesDegree = Math.trunc((minutes / 60) * 360);
+	const hourDegree = Math.trunc((hours / 12) * 360);
+
 	secondHand.style.transform = `rotate(${secondsDegree - 90}deg)`;
-	console.log(seconds);
+	minuteHand.style.transform = `rotate(${minutesDegree - 90}deg)`;
+	hourHand.style.transform = `rotate(${hourDegree - 90}deg)`;
 }
-setInterval(animateSecondHand, 1000);
+setInterval(analogClock, 1000);
